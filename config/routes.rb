2b17784e9f3cde0175042c342b7d3ad78bf7e885
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-11-08 18:23:15 +0000

    User sign up page display

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,21 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  get'/login'=>'session#view'
+  post'/login'=>'session#create'
+  get'/logout'=>'session#destroy'
+
+  get'/signup'=>'users#new'
+  post'/users'=>'users#create'
+
+
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :categories, :products, except: [:edit, :update, :show]
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
