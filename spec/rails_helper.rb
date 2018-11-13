dd0e553753bb0a427406da46b7c3fe8fe94e165e
Konstantin Toroshchin <k.toroshchin@gmail.com>
2018-11-13 18:35:45 +0000

    Commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,10 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require "capybara/rails"
+require "capybara/rspec"
+require "capybara/poltergeist"
+Capybara.javascript_driver = :poltergeist
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -38,6 +41,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = false
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
